4683eaa48354b60cdb0abb9f154bbcd6bcdd9d36
ivanbaguswidodo <mrivanganteng209@gmail.com>
2023-05-14 04:13:53 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,26 @@ import ProductDisplay from './components/ProductDisplay.vue';
 <template>
   <header>
     <!-- card position absolute -->
+    <ProductDisplay>
+      
+    </ProductDisplay>
   </header>
 
   <main>
-    <ProductDisplay>
-      <template #headtop>Head Top</template>
-      <template #headbottom>Head Bottom</template>
-    </ProductDisplay>
+    <div class="headtop-container"/>
+    <div class="headbottom-container"/>
   </main>
 </template>
 
 <style scoped>
+    .headtop-container {
+        background-image: url('../assets/bg-pattern.svg');
+
+        /* background-color: red; */
+        height: 70vh;
+    }
+    .headbottom-container {
+        background-color: #FFFF;
+        height: 30vh;
+    }
 </style>
--- src/components/ProductDisplay.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <slot name="headtop"></slot>
-    <slot name="headbottom"></slot>
-</template>
