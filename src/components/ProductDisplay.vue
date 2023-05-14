ef06483bc4a500971845d2f5128a0aa9510c0c53
ivanbaguswidodo <mrivanganteng209@gmail.com>
2023-05-14 11:52:18 +0700

    final project

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -1,101 +1,102 @@
+
+
 <template>
     <div class="product-display">
-        <div class="kanan-container">
-            <img src="../assets/intersect.png" alt="img">
-        </div>
-        <div class="kiri-container">
-            <h1>Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket</h1>
-            <div>
-                <div class="detail-container">
-                    <p>women's clothing</p>
-                    <div class="rate-container">
-                        <p>2.9/5</p>
-                        <div class="rate-img-container">
-                            <img src="../assets/ellipse-isi.png" alt="rate-1">
-                            <img src="../assets/ellipse-isi.png" alt="rate-2">
-                            <img src="../assets/ellipse-isi.png" alt="rate-3">
-                            <img src="../assets/ellipse-kosong.png" alt="rate-4">
-                            <img src="../assets/ellipse-kosong.png" alt="rate-5">
-                        </div>
-                    </div>
-                </div>
+      <div class="kanan-container">
+        <img :src="currentProduct ? currentProduct.image : ''" alt="img">
+      </div>
+      <div class="kiri-container">
+        <h1>{{ currentProduct ? currentProduct.title : '' }}</h1>
+        <div>
+          <div class="detail-container">
+            <p>{{ currentProduct ? currentProduct.category : '' }}</p>
+            <div class="rate-container">
+              <p>{{ currentProduct ? currentProduct.rating.rate : '' }}/5</p>
+              
+              <div class="rate-img-container">
+                <img
+                  v-for="index in 5"
+                  :key="index"
+                  :src="getRateImage(index)"
+                  :alt="`rate-${index}`"
+                />
                 
-                <hr>
-                <div class="description">
-                    <p>
-                        100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON
-                    </p>
-                </div>
-                <hr>
-                <div>
-                    <h1>$29.95</h1>
-                    <div class="button-container">
-                        <a href="https://www.github.com" class="btn-buy">Buy Now</a>
-                        <button @click="clickedNextButton()" class="btn-next">Next Product</button>
-                        
-                    </div>
-                </div>
+              </div>
             </div>
+          </div>
+          <hr>
+          <div class="description">
+            <p>{{ currentProduct ? currentProduct.description : '' }}</p>
+          </div>
+          <hr>
+          <div>
+            <h1>{{ currentProduct ? `$${currentProduct.price}` : '' }}</h1>
+            <div class="button-container">
+              <a :href="currentProduct ? currentProduct.url : ''" class="btn-buy">Buy Now</a>
+              <button @click="clickedNextButton()" class="btn-next">Next Product</button>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-</template>
+  </template>
 
 <script>
-    import axios from "axios";
+import axios from "axios";
+import ellipseIsi from "@/assets/ellipse-isi.png";
+import ellipseKosong from "@/assets/ellipse-kosong.png";
 
-    const http = axios.create({
-        baseURL: "https://fakestoreapi.com/",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
+const http = axios.create({
+  baseURL: "https://fakestoreapi.com/",
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
 
-    export default {
-        name: "productDisplay",
-        data() {
-            return {
-                currentProduct: null,
-                message : ``,
-                id: 1
-            };
-        },
-        methods: {
-            getProduct(id) {
-                return http
-                .get(`/products/${id}`)
-                .then(res => {
-                    this.currentProduct = res.data;
-                    console.log(res.data);
-                })
-                .catch(e => {
-                    console.log(e);
-                })
-            },
-            clickedNextButton() {
-                this.id += 1;
-                console.log("clicked")
-                console.log("id = " + this.id)
-            }
-            
-        },
-        mounted() {
-            this.message = '';
-            this.getProduct(this.id);
-        }
-    }
-
+export default {
+  name: "productDisplay",
+  data() {
+    return {
+      currentProduct: null,
+      id: 1
+    };
+  },
+  methods: {
     
-
-    // const data = new EcommerceDataService();
-    // data.get(1)
-    // .then(res => {
-    //     console.log(res);
-    // })
+    getProduct(id) {
+      return http
+        .get(`/products/${id}`)
+        .then(res => {
+          this.currentProduct = res.data;
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    clickedNextButton() {
+      this.id += 1;
+  if (this.id > 20) {
+    this.id = 1;
+  }
+  this.getProduct(this.id);
+},
+    getRateImage(index) {
+  if (this.currentProduct && index <= this.currentProduct.rating.rate) {
+    return ellipseIsi;
+  } else {
+    return ellipseKosong;
+  }
+}
+  },
+  mounted() {
+    this.getProduct(this.id);
+  }
+};
 </script>
 
-<style scope>
-    
-    .button-container .btn-buy {
+<style scoped>
+
+.button-container .btn-buy {
         margin-right: 15px;
         background-color: #720060;
         color: #FFFF;
@@ -104,6 +105,7 @@
         font-weight: 600;
         padding: 8px 70px;
     }
+
     .button-container .btn-next {
         border-radius: 5px;
         background-color: #FFFF;
@@ -113,26 +115,6 @@
         color: #720060;
         border-color: #720060;
         border: 3px solid #720060;
-    }
-    .button-container {
-        
-        font-weight:bold;
-    }
-    .detail-container {
-        display: flex;
-        justify-content: space-between;
-    }
-    .rate-container {
-        display: flex;
-    }
-    .rate-container img {
-        margin: 0px 2px;
-    }
-    .rate-container p {
-        margin-right: 10px;
-    }
-    .rate-img-container img{
-        padding: 16px 0px;
     }
     .product-display {
         border-radius: 10px;
@@ -157,7 +139,59 @@
         color: #720060;
     }
 
-    .kiri-container {
-        margin: 40px;
+    .detail-container {
+        display: flex;
+        justify-content: space-between;
     }
+
+    .rate-container {
+        display: flex;
+    }
+    .rate-container img {
+        margin: 0px 2px;
+    }
+    .rate-container p {
+        margin-right: 10px;
+    }
+    .rate-img-container img{
+        padding: 16px 0px;
+    }
+
+
+.kiri-container {
+  margin: 40px;
+}
+
+@media screen and (max-width: 768px) {
+  .product-display {
+    flex-direction: column;
+    padding: 30px;
+  }
+
+  .kanan-container {
+    margin-bottom: 30px;
+    text-align: center;
+  }
+
+  .kiri-container {
+    margin: 0;
+  }
+}
+
+@media screen and (max-width: 576px) {
+  .product-display {
+    width: 90%;
+    padding: 20px;
+  }
+
+  .button-container {
+    flex-direction: column;
+    align-items: center;
+    margin-top: 20px;
+  }
+
+  .button-container .btn-buy {
+    font-weight:bold;
+  }
+}
 </style>
